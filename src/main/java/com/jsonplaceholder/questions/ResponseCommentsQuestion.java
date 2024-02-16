e79bd1d8101632acda62362a357ef838f5cd0db6
Daniela Carolina  Segovia Perdomo <daniela.segovia@sqasa.co>
2024-02-16 01:29:55 -0500

    Se eliminan comentarios

--- src/main/java/com/jsonplaceholder/questions/ResponseCommentsQuestion.java
+++ src/main/java/com/jsonplaceholder/questions/ResponseCommentsQuestion.java
@@ -5,21 +5,12 @@ import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class ResponseCommentsQuestion implements Question<ResponseCommentsModel> {
     @Override
     public ResponseCommentsModel answeredBy(Actor actor) {
         return SerenityRest.lastResponse().jsonPath()
                 .getObject("data",ResponseCommentsModel.class);
-        //return Arrays.asList(SerenityRest.lastResponse().as(ResponseCommentsModel[].class));
     }
 
-    /*
-    public static ResponseCommentsQuestion all(){
-        return new ResponseCommentsQuestion();
-    }
-
-     */
 }
--- src/test/java/com/jsonplaceholder/stepDefinitions/JsonCommentsStepDef.java
+++ src/test/java/com/jsonplaceholder/stepDefinitions/JsonCommentsStepDef.java
@@ -1,17 +1,13 @@
 package com.jsonplaceholder.stepDefinitions;
 
-import com.jsonplaceholder.questions.ResponseCommentsQuestion;
 import com.jsonplaceholder.tasks.GetCommentsByIdTask;
 import io.cucumber.java.en.*;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static org.hamcrest.Matchers.equalTo;
 
 public class JsonCommentsStepDef {
 
-    //Actor que se usará
     Actor manage = Actor.named("manage")
             .whoCan(CallAnApi.at("https://jsonplaceholder.typicode.com"));
 
@@ -24,15 +20,5 @@ public class JsonCommentsStepDef {
     }
     @Then("I will be able to view the comments available for the desired PostId")
     public void iWillBeAbleToViewTheCommentsAvailableForTheDesiredPostId() {
-        /*
-        manage.should(
-                seeThat(
-                        "The email of comments is",
-                        res -> new ResponseCommentsQuestion().answeredBy(manage).get(0).getEmail(),
-                        equalTo("Presley.Mueller@myrl.com")
-                )
-        );
-
-         */
     }
 }
